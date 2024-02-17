dc9db740afb82f8e1c845aa15dc12f44c1d1c714
SebastienWeb <sebastienweb27@gmail.com>
2024-02-17 13:19:53 +0100

    ✅ Update HeroBanner component with responsive background images

--- components/layout/HeroBanner.jsx
+++ components/layout/HeroBanner.jsx
@@ -1,12 +1,22 @@
 import { Button } from "@/components/ui/button.jsx";
+import Image from "next/image.js";
+import svgMobile from "/public/assets/images/background/mobile.svg";
 
 export function HeroBanner() {
   return (
     <section>
       <div className="relative h-[475px]">
         <div className="background"></div>
-        <div className="absolute w-full pb-12 top-1/2 left-1/2 container -translate-y-1/2 -translate-x-1/2 md:text-center lg:text-left">
-          <h1 className="320:text-2xl 388:text-4xl lg:text-6xl sm:text-center lg:text-left">
+        <picture className=" w-full absolute -bottom-2 md:-bottom-8 lg:-bottom-0 block">
+          <source
+            srcSet="/assets/images/background/desktop.svg"
+            media="(min-width: 1024px)"
+          />
+          <Image src={svgMobile} alt="" className="w-full" />
+        </picture>
+
+        <div className="absolute w-full pb-12 md:pb-32 top-1/2 left-1/2 container -translate-y-1/2 -translate-x-1/2 md:text-center lg:text-left">
+          <h1 className="320:text-2xl 388:text-4xl md:text-5xl lg:text-7xl text-left">
             La Petite Boutique D&apos;Ephée
           </h1>
           <p className="mt-7 text-lg lg:ml-0 text-left text-pretty">
@@ -15,7 +25,7 @@ export function HeroBanner() {
             Des créations authentiques, mêlant originalité et qualité
             artisanale.
           </p>
-          <div className="mt-16 flex justify-between gap-8 flex-wrap 320:mt-8 sm:justify-center lg:justify-start">
+          <div className="mt-16 flex 320:mt-8 justify-start">
             <Button variant="secondary" size="default">
               Nos produits
             </Button>
