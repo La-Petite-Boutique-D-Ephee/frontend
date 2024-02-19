23c7188e738a25284a785bb06db418c88ae30866
SebastienWeb <sebastienweb27@gmail.com>
2024-02-19 16:49:24 +0100

    ✅  Update logo, navigation, and login button to header

--- components/layout/Header.jsx
+++ components/layout/Header.jsx
@@ -1,16 +1,30 @@
+import { Button } from "@/components/ui/button.jsx";
 import logo from "@/public/assets/images/logo/logo.png";
+import { User } from "lucide-react";
 import Image from "next/image.js";
+import { Desktop } from "./navigation/Desktop.jsx";
+import { Mobile } from "./navigation/Mobile.jsx";
 
 export function Header() {
   return (
     <header className="bg-background-500/70 backdrop-blur-sm pt-2 pb-6 shadow-md sticky top-0 z-50">
-      <Image
-        src={logo}
-        alt="Logo de La Petite Boutique D'Ephée"
-        width={124}
-        height={124}
-        priority={true}
-      />
+      <div className="sm:container flex justify-between items-center">
+        <div>
+          <Image
+            src={logo}
+            alt="Logo de La Petite Boutique D'Ephée"
+            width={124}
+            height={124}
+            priority={true}
+          />
+        </div>
+        <Mobile />
+        <Desktop />
+        <Button className="hidden lg:inline-flex">
+          <User size={20} className="mr-4" />
+          Connexion
+        </Button>
+      </div>
     </header>
   );
 }
