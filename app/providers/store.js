d1d1f331af81e7915778ee5a1d8cd328dcc36feb
SebastienWeb <sebastienweb27@gmail.com>
2024-03-05 12:15:28 +0100

    ✨ Add Zustand store and jwt-decode dependency

---
+++ app/providers/store.js
@@ -0,0 +1,18 @@
+import { create } from "zustand";
+import { getToken } from "../action.js";
+
+export const useStore = create((set) => ({
+  user: null,
+  fetchUser: async () => {
+    const response = await fetch("https://127.0.0.1:8000/api/me", {
+      method: "GET",
+      headers: {
+        Authorization: `Bearer ${await getToken()}`,
+        "Content-Type": "application/json",
+      },
+    });
+    const data = await response.json();
+    set({ user: data.data.user });
+    return data.data.user;
+  },
+}));
