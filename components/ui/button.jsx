bbefd2a00072e2f9253cf9d3a06705059b0a564d
SebastienWeb <sebastienweb27@gmail.com>
2024-03-05 12:16:01 +0100

    ✅ Update button variants

--- components/ui/button.jsx
+++ components/ui/button.jsx
@@ -10,13 +10,13 @@ const buttonVariants = cva(
     variants: {
       variant: {
         default:
-          "bg-primary-500 text-slate-50 hover:bg-primary-500/90  transition-colors duration-300 ease-in-out",
+          "bg-primary-500 text-white hover:bg-primary-500/90  transition-colors duration-300 ease-in-out",
         destructive:
-          "bg-red-500 text-slate-50 hover:bg-red-500/90 dark:bg-red-900 dark:text-slate-50 dark:hover:bg-red-900/90 transition-colors duration-300 ease-in-out",
+          "bg-red-500 text-white hover:bg-red-500/90 dark:bg-red-900 dark:text-slate-50 dark:hover:bg-red-900/90 transition-colors duration-300 ease-in-out",
         outline:
           "border border-slate-200 bg-white hover:bg-slate-100 hover:text-slate-900 dark:border-slate-800 dark:bg-slate-950 dark:hover:bg-slate-800 dark:hover:text-slate-50",
         secondary:
-          "bg-secondary-500 text-btn hover:bg-secondary-500/80 transition-colors duration-300 ease-in-out",
+          "bg-secondary-500 text-white hover:bg-secondary-500/80 transition-colors duration-300 ease-in-out",
         ghost:
           "hover:bg-slate-100 hover:text-slate-900 dark:hover:bg-slate-800 dark:hover:text-slate-50",
         link: "text-slate-900 underline-offset-4 hover:underline dark:text-slate-50",
