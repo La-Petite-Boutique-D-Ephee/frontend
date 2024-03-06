6dc25d47786c5654b3f7385aaed01e3a47b8441a
SebastienWeb <sebastienweb27@gmail.com>
2024-03-06 16:26:32 +0100

    🐛 Fix import selectionOfMoment

--- app/page.js
+++ app/page.js
@@ -1,11 +1,11 @@
 import { Header } from "../components/layout/Header.jsx";
 import { HeroBanner } from "../components/layout/HeroBanner.jsx";
 import { Reseaux } from "../components/layout/Reseaux.jsx";
-import { SelectionOfMoment } from "../components/layout/SelectionOfMoment/SelectionOfMoment.jsx";
 import { SommeNous } from "../components/layout/SommeNous.jsx";
 import { Trust } from "../components/layout/Trust.jsx";
 import { Category } from "../components/layout/category/Category.jsx";
 import { Footer } from "../components/layout/footer/Footer.jsx";
+import { SelectionOfMoment } from "../components/layout/selectionOfMoment/SelectionOfMoment.jsx";
 import { Articles } from "../components/layout/slider/Articles.jsx";
 
 export default function Home() {
